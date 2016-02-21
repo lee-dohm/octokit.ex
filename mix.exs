4a2fb6868245ba3aae2240cced61808ab3ae2440
Lee Dohm <lee-dohm@github.com>
2016-02-20 20:24:45 -0800

    :bug: Fix syntax error

--- mix.exs
+++ mix.exs
@@ -72,7 +72,7 @@ defmodule Octokit.Mixfile do
 
   defp package do
     [
-      files: ["lib", "mix.exs", "*.md"]
+      files: ["lib", "mix.exs", "*.md"],
       maintainers: ["Lee Dohm"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/lee-dohm/octokit.ex"}
